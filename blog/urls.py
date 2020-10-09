ed7e96fe101009c11e12891ae9c833cf665a1587
Kathlia Carlos <lia.carlos23@gmail.com>
2020-10-09 10:53:46 +0800

    add Blogeditview page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
 
-from .views import HomePageView, BlogListView, BlogCreateView
+from .views import HomePageView, BlogListView, BlogCreateView, BlogEditView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('post/<int:pk>', BlogListView.as_view(), name='post_detail'),
     path('post/new', BlogCreateView.as_view(), name='post_new'),
+    path('post/edit/<int:pk>', BlogEditView.as_view(), name='post_edit'),
 ]
