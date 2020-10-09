fdfec63330786e379060242615fbbb652257ed65
Kathlia Carlos <lia.carlos23@gmail.com>
2020-10-09 11:07:53 +0800

    add blogDeleteView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Post
 # Create your views here.
@@ -25,3 +26,9 @@ class BlogEditView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'text']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home') # will be redirected after the post is deleted, not immediately when using reverse only
